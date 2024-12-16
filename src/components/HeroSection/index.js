52a10a7c332ad5499e674653144127f538f24c5f
Vikas Saxena <78485682+VikasSaxena2204@users.noreply.github.com>
2024-12-16 10:36:43 +0530

    Update index.js

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -13,7 +13,7 @@ import {
     SubTitle, 
     ResumeButton 
 } from './HeroStyle';
-import HeroImg from '../../images/Vikas.jpg';
+import HeroImg from '../../images/Vikas_pic1.jpg';
 import Typewriter from 'typewriter-effect';
 import { Bio } from '../../data/constants';
 
