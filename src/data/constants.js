da5aa18db181fdaee4078f1443016cf67418bd47
Vikas Saxena <78485682+VikasSaxena2204@users.noreply.github.com>
2025-01-04 13:12:28 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,5 +1,5 @@
 
-// Imports Portfolio Images 
+// Imports Images 
 import quickConvoImage from '../images/QUICKCONVO.png';
 import studentManagementImage from '../images/SMS.png';
 import newsStreamImage from '../images/NewsStream.png';
