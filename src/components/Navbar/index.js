2099182ef9742dbc21c1ee165f7b478aeb2b5815
Vikas Saxena <78485682+VikasSaxena2204@users.noreply.github.com>
2026-06-25 18:28:27 +0530

    Update index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -26,11 +26,20 @@ const Navbar = () => {
   return (
     <Nav>
       <NavbarContainer>
-        <NavLogo to='/'>
-          <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20', cursor: 'pointer' }}>
-            <DiCssdeck size="3rem" /> <Span> My Portfolio</Span>
-          </a>
-        </NavLogo>
+       <NavLogo to='/'>
+  <div
+    style={{
+      display: "flex",
+      alignItems: "center",
+      color: "white",
+      marginBottom: "20px",
+      cursor: "pointer"
+    }}
+  >
+    <DiCssdeck size="3rem" />
+    <Span>My Portfolio</Span>
+  </div>
+</NavLogo>
         
         <MobileIcon onClick={handleMenuToggle}>
           <FaBars />
